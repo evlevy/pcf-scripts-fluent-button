adf120927d655eded15c1b176655b99c620313e2
Evan Levy <evlevy@microsoft.com>
2022-11-22 23:22:29 +0000

    Update 3 files

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,9 @@
 module.exports = {
   mode: "production",
-  externals: [
-    {
-      react: "React",
-      "react-dom": "ReactDOM",
-    },
-  ],
+  // externals: [
+  //   {
+  //     react: "React",
+  //     "react-dom": "ReactDOM",
+  //   },
+  // ],
 };
